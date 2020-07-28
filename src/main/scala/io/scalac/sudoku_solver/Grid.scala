1c040c8d36a3a4686f33ca45c7ffdee21e9e8f2f
Mariusz Nosinski <marioosh@5dots.pl>
2020-07-28 17:44:30 +0200

    before optimalization

--- src/main/scala/io/scalac/sudoku_solver/Grid.scala
+++ src/main/scala/io/scalac/sudoku_solver/Grid.scala
@@ -1,5 +1,7 @@
 package io.scalac.sudoku_solver
 
+import scala.annotation.tailrec
+
 case class Grid[A](values: Vector[Vector[A]]) {
 
   def updated(x: Int, y: Int)(value: A) =
@@ -14,9 +16,46 @@ case class Grid[A](values: Vector[Vector[A]]) {
 
 object Grid {
 
+  def withColumns[A](grid: Grid[A]): Grid[A] = {
+    val indexes = for {
+      i <- 0 until SIZE
+      j <- 0 until SIZE
+    } yield i -> j
+
+    indexes
+      .toList
+      .foldLeft(grid.copy()) { case (newGrid, (i, j)) =>
+        newGrid.updated(j, i)(grid.getValue(i, j))
+      }
+  }
+
+  def withSectors[A](grid: Grid[A]): Grid[A] = {
+    val indexes = for {
+      i <- 0 until 3
+      j <- 0 until 3
+      k <- 0 until 3
+    } yield (i, j, k)
+
+    indexes
+      .toList
+      .foldLeft(grid.copy()) {
+        case (newGrid, (i, j, k)) =>
+
+          newGrid
+            .updated(j + k * 3, i)(grid.getValue(k * 3, i + j * 3))
+            .updated(j + k * 3, i + 3)(grid.getValue(1 + k * 3, i + j * 3))
+            .updated(j + k * 3, i + 6)(grid.getValue(2 + k * 3, i + j * 3))
+      }
+
+  }
+
   val SIZE: Int = 9
 
   def fill[A](values: Vector[Vector[A]]) = Grid(values)
 
   def fill[A](value: A) = Grid(Vector.fill(SIZE, SIZE)(value))
+
+  def display[A](grid: Grid[A]): String = {
+    grid.values.map(_.mkString("\t", "  ", "\t")).mkString("\n")
+  }
 }
--- src/main/scala/io/scalac/sudoku_solver/Main.scala
+++ src/main/scala/io/scalac/sudoku_solver/Main.scala
@@ -1,11 +1,47 @@
 package io.scalac.sudoku_solver
 
-import org.chocosolver.solver.variables.IntVar
+import org.chocosolver.solver.Model
 
 object Main extends App {
 
-  println("Welcome to Sudoku Solver\tLet's solve sample puzzle")
+  println("Welcome to Sudoku Solver\nLet's solve sample puzzle")
 
-  val cols, rows, sectors = Vector[Vector[IntVar]]()
+  val grid = Grid(Vector(
+    Vector(0, 0, 0, 2, 0, 0, 0, 0, 0),
+    Vector(0, 8, 0, 0, 3, 0, 0, 7, 0),
+    Vector(3, 0, 0, 5, 0, 4, 0, 0, 0),
+    Vector(0, 0, 0, 0, 0, 0, 0, 2, 8),
+    Vector(8, 3, 0 ,0, 1, 0, 0, 0, 0),
+    Vector(0, 4, 0, 7, 2, 0, 3, 5, 1),
+    Vector(0, 7, 0, 0, 5, 6, 0, 0, 4),
+    Vector(0, 0, 3, 0, 0, 0, 0, 0, 0),
+    Vector(2, 0, 5, 4, 0, 1, 6, 0, 3)
+  ))
+
+  val model = new Model("Sudoku solver")
+
+  val rows = grid.map {
+    case 0 => model.intVar(1, 9)
+    case n => model.intVar(n)
+  }
+
+  val cols = Grid.withColumns(rows)
+  val sectors = Grid.withSectors(rows)
+
+  val allConstraints = rows.values ++ cols.values ++ sectors.values
+
+
+  allConstraints.foreach { contraints =>
+    model.post(model.allDifferent(contraints: _*))
+  }
+  val solver = model.getSolver
+
+  if(solver.solve()){
+    println(s"Solved!")
+    println(Grid.display(rows.map(_.getValue())))
+
+  } else {
+    println("Didn't find any solution! :/")
+  }
 
 }
